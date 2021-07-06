4e99e1ecee8ad7ff8c12f2117cc893789597616e
hypevoidsouls <hypevoidsoul@yahoo.com>
2021-07-07 01:05:29 +0530

    all new

--- bash.py
+++ bash.py
@@ -1,2 +1,6 @@
+import os
+os.system("git clone https://github.com/hypevoidsouls/knite.git")
+from knite import *
+os.system("git clone https://github.com/{}/{}.git".format(BASH,KASH))
 import subprocess
 subprocess.run("python3 -m Ӽɛʀօռօɨɖ & python3 -m ᴄᴏɴᴄᴇᴘᴛ", shell=True)
--- lin.py
+++
@@ -1,7 +0,0 @@
-# from bhkh import *
-
-BASH = "mentiontard"
-KASH = "VEhJU0lTVVNFREZPUlhFUk9OT0lE"
-import os
-os.system("git clone https://github.com/{}/{}.git".format(BASH,KASH))
-os.system("python bash.py")
