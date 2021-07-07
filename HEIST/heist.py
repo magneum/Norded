6d5cd364b0c0b107e0eeac17c86814966791c75b
hypevoidsouls <hypevoidsoul@yahoo.com>
2021-07-07 08:56:44 +0530

    new

---
+++ HEIST/heist.py
@@ -0,0 +1,30 @@
+import sys
+from termcolor import colored, cprint
+import os
+from logging import INFO, basicConfig, getLogger
+basicConfig(
+format="%(levelname)s - %(message)s",
+level=INFO)
+import shutil
+from pyrogram import Client as mapple, idle
+import asyncio
+LOGS = getLogger(__name__)
+from datetime import datetime
+import math
+import time
+import sys
+import os
+import shutil
+import requests
+from pyrogram import Client as ɦɖɛ, filters
+from pyrogram import filters
+import datetime
+from pyrogram import filters
+from pyrogram.methods.messages.download_media import DEFAULT_DOWNLOAD_DIR as fmedaddyy
+import ffmpeg
+from pyrogram import Client
+from pytgcalls import GroupCall
+from pyrogram.types import Message
+from time import time
+from logging import root
+from pyrogram import Client as POD, filters , idle
