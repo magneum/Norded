74f264a9178eb9aaf1d43e485f766530f00fdadd
hypevoidsouls <hypevoidsoul@yahoo.com>
2021-07-07 12:27:23 +0530

    qwd

--- ᴀᴜɴᴇʀ/ᴀᴜɴᴇʀ.py
+++ ᴀᴜɴᴇʀ/ᴀᴜɴᴇʀ.py
@@ -5,7 +5,7 @@ class ɢʟɛǟʍ:
     SN = os.environ["SESSION_NAME"]
     DYNO_COMMAND = os.environ["DYNO_COMMAND"]  
     WHITE_COMMAND = os.environ["WHITE_COMMAND"] 
-    XERONOID_KEY = os.environ["XERONOID_KEY"]
+    XERONOID = os.environ["XERONOID_KEY"]
     HEROKU_APP_NAME = os.environ["HEROKU_APP_NAME"]
     HEROKU_API_KEY = os.environ["HEROKU_API_KEY"]
     OWNER_USERNAME = os.environ["OWNER_USERNAME"]
