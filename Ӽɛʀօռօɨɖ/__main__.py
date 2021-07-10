cdd542ce4a12eb6b79ca1e4982e301b03263355a
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-07-10 07:31:58 -0700

    Update __main__.py

--- Ӽɛʀօռօɨɖ/__main__.py
+++ Ӽɛʀօռօɨɖ/__main__.py
@@ -4,8 +4,13 @@ from knite import *
 Ӽɛռ.start()
 LOGS.info(XEon)
 idle()
-Ӽɛռ.stop()
-shutil.rmtree(K)
-shutil.rmtree(P)
-shutil.rmtree(V)
+Ӽɛռ.stop()   
+try:
+    shutil.rmtree(K)
+    shutil.rmtree(P)
+    shutil.rmtree(V)
+    shutil.rmtree(Y)
+    shutil.rmtree(M)
+except:
+    pass
 LOGS.info(XEof)
