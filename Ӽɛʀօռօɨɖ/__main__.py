22175b4080f37bf679060ade430d0415edad1a24
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-07-10 23:21:26 -0700

    from pyrogram.methods.utilities import idle

--- Ӽɛʀօռօɨɖ/__main__.py
+++ Ӽɛʀօռօɨɖ/__main__.py
@@ -1,4 +1,16 @@
-from . import *
+from HEIST import *
+from VEhJU0lTVVNFREZPUlhFUk9OT0lE import *
+
+Ӽɛռ = mapple(
+    session_name=SN,
+    api_id=AD,
+    api_hash=AH,
+    plugins=ᴄᴇᴘᴛ,
+    workers=20
+    )
+from ᴇᴘɪꜱᴛʟᴇ import *
+from pyrogram.methods.utilities import idle
+
 from knite import *
 
 Ӽɛռ.start()
