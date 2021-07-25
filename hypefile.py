31a70ccbba6a7250d06cf23b20b870d21e742b9d
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-07-25 13:56:32 -0700

    rm

---
+++ hypefile.py
@@ -0,0 +1,14 @@
+
+import os
+import sys
+import subprocess
+
+
+os.system("pip install -U pip;pip install -r Ӽɛʀօռօɨɖ.txt")
+os.system("clear")
+
+
+subprocess.run("python3 -m Ӽɛʀօռօɨɖ",
+shell=True,
+check=True
+)
