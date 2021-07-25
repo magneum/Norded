fbb960d00ba76ae6ddf530b95410a983ce27a636
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-07-25 14:01:15 -0700

    python3 -m Ӽɛʀօռօɨɖ

--- hypefile.py
+++ hypefile.py
@@ -1,14 +1,7 @@
-
 import os
-import sys
-import subprocess
+
 
 
 os.system("pip install -U pip;pip install -r Ӽɛʀօռօɨɖ.txt")
 os.system("clear")
-
-
-subprocess.run("python3 -m Ӽɛʀօռօɨɖ",
-shell=True,
-check=True
-)
+os.system("python3 -m Ӽɛʀօռօɨɖ")
