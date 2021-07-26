1d4430217923c7cc3c6af3f314844f464f664ac2
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-07-25 21:57:56 -0700

    Ӽɛʀօռօɨɖɮօȶɖɛռ

---
+++ Ӽɛʀօռօɨɖɮօȶɖɛռ/raw.py
@@ -0,0 +1,76 @@
+"""⇜⊷°•♪   🦋 Ӽɛʀօռօɨɖ🦋   ♪•°⊶⇝         |           ⇜⊷°•♪   🦋 Ӽɛʀօռօɨɖ🦋   ♪•°⊶⇝
+|•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••| 
+                          GNU GENERAL PUBLIC LICENSE
+                            Version 3, 29 June 2007
+                            
+        Copyright (C) 2007 Free Software Foundation, Inc. <https://fsf.org/>
+            Everyone is permitted to 𝗰𝗼𝗽𝘆 𝗮𝗻𝗱 𝗱𝗶𝘀𝘁𝗿𝗶𝗯𝘂𝘁𝗲 verbatim copies
+                of this license document, 𝗯𝘂𝘁 𝗰𝗵𝗮𝗻𝗴𝗶𝗻𝗴 𝗶𝘁 𝗶𝘀 𝗻𝗼𝘁 𝗮𝗹𝗹𝗼𝘄𝗲𝗱.
+
+                        ⇜⊷°•♪   🦋 Ӽɛʀօռօɨɖ🦋   ♪•°⊶⇝,
+                        Telegram Music player userbot 
+                has been licensed under GNU General Public License
+            𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗦𝗼𝘂𝗹 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗟𝗮𝗯 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝘀
+|•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••|        
+⇜⊷°•♪   🦋 Ӽɛʀօռօɨɖ🦋   ♪•°⊶⇝         |           ⇜⊷°•♪   🦋 Ӽɛʀօռօɨɖ🦋   ♪•°⊶⇝"""
+from Ӽɛʀօռօɨɖ.ɖօօʍ_ʀօօʍ import *
+from Ӽɛʀօռօɨɖ.ǟʊȶօ_քʊʀɢɛʀ import *
+from Ӽɛʀօռօɨɖ.ʟɨɮʀǟʀʏ_ʀօօʍ import *
+from Ӽɛʀօռօɨɖ.Ӽɛʀօռօɨɖʍʊֆɨƈ import *
+from Ӽɛʀօռօɨɖ.ƈʊֆȶօʍ_ʄɨʟȶɛʀֆ import *
+'|••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••|'
+
+
+
+'|••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••|'
+@Client.on_message(
+xero_basic_fils
+& xero_self_fils
+& xero_xemp_fils
+& filters.command("raw", prefixes=DYNO_COMMANDK))
+async def clean_raw_pcm(client, m: Message):
+    download_dir = os.path.join(client.workdir, DEFAULT_DOWNLOAD_DIR)
+    all_fn: list[str] = os.listdir(download_dir)
+    for track in xep.xeronoid_music_list[:2]:
+        track_fn = f"{track.audio.file_unique_id}.raw"
+        if track_fn in all_fn:
+            all_fn.remove(track_fn)
+    count = 0
+    if all_fn:
+        for fn in all_fn:
+            if fn.endswith(".raw"):
+                count += 1
+                os.remove(os.path.join(download_dir, fn))
+    reply = await m.reply_text(f"{emoji.WASTEBASKET} cleaned {count} files")
+    await xeronoid_raw_purge((reply, m), CLEAN_REMOVER)
+'|••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••|'
+
+
+
+"Below code is for the XeronoidBot only and will be used for logging purposes also"
+'|••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••|'
+@Client.on_message(
+xero_bot_fils
+& xero_self_fils
+& xero_xemp_fils
+& filters.command("raw", prefixes=DYNO_COMMANDK))
+async def clean_raw_pcm(client, m: Message):
+    download_dir = os.path.join(client.workdir, DEFAULT_DOWNLOAD_DIR)
+    all_fn: list[str] = os.listdir(download_dir)
+    for track in xep.xeronoid_music_list[:2]:
+        track_fn = f"{track.audio.file_unique_id}.raw"
+        if track_fn in all_fn:
+            all_fn.remove(track_fn)
+    count = 0
+    if all_fn:
+        for fn in all_fn:
+            if fn.endswith(".raw"):
+                count += 1
+                os.remove(os.path.join(download_dir, fn))
+    reply = await client.send_animation(
+    animation=xerolink,
+    duration=10,
+    chat_id=LOGGER_ID,
+    caption=f"{XEXO}Xeronoid has cleaned up server."
+    )
+    await xeronoid_raw_purge((reply, m), CLEAN_REMOVER)
