137c76d44f9e6649a1122af3cfdc3e4648d689ab
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-07-25 23:54:54 -0700

    🦋 Ӽɛʀօռօɨɖ🦋

--- ƈʊֆȶօʍ_ʄɨʟȶɛʀֆ/pyro.py
+++ ƈʊֆȶօʍ_ʄɨʟȶɛʀֆ/pyro.py
@@ -22,8 +22,7 @@ from Ӽɛʀօռօɨɖʍʊֆɨƈ.xmp import xep
 
 xero_self_fils = filters.create(
 lambda _, __, message:
-(message.from_user and message.from_user.is_contact)
-or message.outgoing)
+(message.from_user and message.from_user.is_contact)or message.outgoing)
 
 
 
