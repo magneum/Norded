24104782d6e20101c16c1f53a3a9aaa4252b9dee
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-07-26 03:02:48 -0700

    dqwd

--- ʟɨɮʀǟʀʏ_ʀօօʍ/api.py
+++ ʟɨɮʀǟʀʏ_ʀօօʍ/api.py
@@ -31,7 +31,7 @@ if HEROKU is not None and HEROKU == "HEROKU":
 elif HEROKU != "HEROKU":
     API_ID = 5397317 
     API_HASH = "7ed80948c3b916010963407eaccd1752"
-    BOT_TOKEN = "1631463971:AAFLcSTXJR11d9Imay6pWJ-7NGijbThH6Ts"
+    BOT_TOKEN = "1929102577:AAH1z39QtqCNjXfGAdRH2k_gm-J1Cp_vArc"
     XERONOID_SESSION = "BQDF2H8tazqK34_7MJS2jTKRId_v3A7EN7lmFUGTSmIJTCyltEsGoTglzsty9XeaMut495fsmlF9KiVszVjSsnkbHYsQjw1dbact5pwTAtnMqXFyo-BqYmYDgFkKz9kKtaPum_3Vuq5e-0N9grgVpvwBM-XPmnqeL1lYwhw1JWXxSHCpG3eGfNfDzJVx8Zjym9bMAcH7cGQjs-RUlcltzSDd-_sQEhA4--TmNA0m85HyWccSS5nYJtAWEEnnUDaxoPJ7zpw0W6KIRl--d0GCEwlUyuHgqR_2_2pJTx40Vr9Qba_DcDYHp1BiFLS3agwpV6ErRGhnYjA-IofGc135L4ErbjAdTQA" 
 else:
     cprint('Please recheck all the needed variables and restart the bot.',on_color='on_red')
