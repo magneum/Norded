0ca54cc4d544841e0e8166b600b9e7784ca15cd2
hypevoidsoul <hypevoidsoul@yahoo.com>
2021-07-27 02:46:23 -0700

    Python3

--- hypefile.py
+++ hypefile.py
@@ -1,4 +1,4 @@
-# This Python file uses the following encoding: utf-8
+# This python3 file uses the following encoding: utf-8
 """⇜⊷°•♪   🦋 Ӽɛʀօռօɨɖ🦋   ♪•°⊶⇝         |           ⇜⊷°•♪   🦋 Ӽɛʀօռօɨɖ🦋   ♪•°⊶⇝
 |•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••| 
                           GNU GENERAL PUBLIC LICENSE
@@ -29,18 +29,18 @@ print("The bot is going to run in"  +   platform.upper())
 HEROKU = os.environ.get('HEROKU')
 if HEROKU == 'HEROKU':
     # When HEROKU, req will be met before this point
-    subprocess.run("python -m Ӽɛʀօռօɨɖ & python -m Ӽɛʀօռօɨɖɮօȶ",
+    subprocess.run("python3 -m Ӽɛʀօռօɨɖ & python3 -m Ӽɛʀօռօɨɖɮօȶ",
     shell=True,
     check=True)
 elif HEROKU != 'HEROKU':
     # This is for replit
     os.system("pip install -U pip;pip install -r Ӽɛʀօռօɨɖ.txt")
     os.system("clear")
-    subprocess.run("python -m Ӽɛʀօռօɨɖ & python -m Ӽɛʀօռօɨɖɮօȶ",
+    subprocess.run("python3 -m Ӽɛʀօռօɨɖ & python3 -m Ӽɛʀօռօɨɖɮօȶ",
     shell=True,
     check=True)
 else:
     # This is to make sure code continues to propagate no matter what
-    subprocess.run("python -m Ӽɛʀօռօɨɖ & python -m Ӽɛʀօռօɨɖɮօȶ",
+    subprocess.run("python3 -m Ӽɛʀօռօɨɖ & python3 -m Ӽɛʀօռօɨɖɮօȶ",
     shell=True,
     check=True)
