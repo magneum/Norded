46a65f1b3e7aaf06676d5e9a318376adf8067baf
hypevoidsoul <hypevoidsoul@yahoo.com>
2021-07-31 01:53:08 +0530

    '⇜⊷°•♪🦋♪•°⊶⇝'

--- ɦʏքɛʋօɨɖƈօքʏʀɨɢɦȶ/ʟɪʙʀᴀʀʏ/variables.py
+++ ɦʏքɛʋօɨɖƈօքʏʀɨɢɦȶ/ʟɪʙʀᴀʀʏ/variables.py
@@ -14,26 +14,44 @@
             𝐂𝐨𝐩𝐲𝐫𝐢𝐠𝐡𝐭 (𝐂) 𝟐𝟎𝟐𝟏 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗦𝗼𝘂𝗹 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝗟𝗮𝗯 | 𝗛𝘆𝗽𝗲𝗩𝗼𝗶𝗱𝘀
 ⇜⊷°•♪   🦋Ӽɛʀօռօɨɖ🦋   ♪•°⊶⇝         |           ⇜⊷°•♪   🦋Ӽɛʀօռօɨɖ🦋   ♪•°⊶⇝
 |•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••|        
-"""
+""" 
 import os
 from termcolor import *
 HEROKU = os.environ.get('HEROKU', None)
-if HEROKU is not None and HEROKU == "HEROKU":   
+if HEROKU is not None and HEROKU == "HEROKU":  
+    '⇜⊷°•♪🦋♪•°⊶⇝'
     GROUP_USERNAME = os.environ.get('GROUP_USERNAME')
     if GROUP_USERNAME:
-        GROUP_USERNAME = GROUP_USERNAME.capitalize()
+        GROUP_USERNAME = GROUP_USERNAME.upper()
+        '⇜⊷°•♪🦋♪•°⊶⇝'
     OWNER_USERNAME = os.environ.get('OWNER_USERNAME')
+    if OWNER_USERNAME:
+        OWNER_USERNAME = OWNER_USERNAME.upper()
+        '⇜⊷°•♪🦋♪•°⊶⇝'
     BOT_USERNAME = os.environ.get('BOT_USERNAME')
+    if BOT_USERNAME:
+        BOT_USERNAME = BOT_USERNAME.upper()
+        '⇜⊷°•♪🦋♪•°⊶⇝'
     MUSIC_ADMIN_USERNAMES = os.environ.get('MUSIC_ADMIN_USERNAMES')
+    if MUSIC_ADMIN_USERNAMES:
+        MUSIC_ADMIN_USERNAMES = MUSIC_ADMIN_USERNAMES.upper()
+        '⇜⊷°•♪🦋♪•°⊶⇝'
     LOGGER_ID_TEMP = os.environ.get('LOGGER_ID')
-    LOGGER_ID = int(LOGGER_ID_TEMP)
+    if LOGGER_ID_TEMP:
+        LOGGER_ID = int(LOGGER_ID_TEMP)
+        '⇜⊷°•♪🦋♪•°⊶⇝'
+    OWNER_ID_TEMP = os.environ.get('OWNER_ID')
+    if OWNER_ID_TEMP:
+        OWNER_ID= int(OWNER_ID_TEMP)
+        '⇜⊷°•♪🦋♪•°⊶⇝'
 else:
     OWNER_ID = [1848646989]
     OWNER_USERNAME = '@HYPEVOIDSOUL'
     BOT_USERNAME = "@XERONOIDBOT"
     LOGGER_ID = int("-1001513582173")
-    GROUP_USERNAME = '@kalocksX'
-    MUSIC_ADMIN_USERNAMES = '@HYPEVOIDSOUL,@DEVLIXIE,@KALITRONX'
+    GROUP_USERNAME = '@kalocksX,@HypeVoids'
+    MUSIC_ADMIN_USERNAMES = '@hypevoidsoul,@DEVLIXIE,@kalitronx'
+'⇜⊷°•♪🦋♪•°⊶⇝'
 XERO = '⇜⊷°•♪   🦋Ӽɛʀօռօɨɖ🦋   ♪•°⊶⇝         |           ⇜⊷°•♪   🦋Ӽɛʀօռօɨɖ🦋   ♪•°⊶⇝'
 MAX_MIN = 8
 MAX_HOUR = 3
