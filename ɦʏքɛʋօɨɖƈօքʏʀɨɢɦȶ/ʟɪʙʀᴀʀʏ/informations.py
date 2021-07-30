ed8e9eccfb6aede4bb95a488a5e883a59b3cf056
hypevoidsoul <hypevoidsoul@yahoo.com>
2021-07-31 03:23:07 +0530

    🦋Ӽɛʀօռօɨɖ🦋

--- ɦʏքɛʋօɨɖƈօքʏʀɨɢɦȶ/ʟɪʙʀᴀʀʏ/informations.py
+++ ɦʏքɛʋօɨɖƈօքʏʀɨɢɦȶ/ʟɪʙʀᴀʀʏ/informations.py
@@ -15,24 +15,25 @@
 ⇜⊷°•♪   🦋Ӽɛʀօռօɨɖ🦋   ♪•°⊶⇝         |           ⇜⊷°•♪   🦋Ӽɛʀօռօɨɖ🦋   ♪•°⊶⇝
 |•••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••••|        
 """
-
 from .variables import XEXO, DYNO_COMMANDK
+
+
 
 XERO_HELP = f"""{XEXO}
 ⇜⊷°•♪  𝗠𝗲𝗺𝗯𝗲𝗿_𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀  ♪•°⊶⇝
-- {DYNO_COMMANDK}sing: ʀᴇᴘʟʏ ᴡɪᴛʜ ᴀɴ ᴀᴜᴅɪᴏ ᴛᴏ ᴘʟᴀʏ/Qᴜᴇᴜᴇ ɪᴛ.
-- {DYNO_COMMANDK}sing: ᴀʟꜱᴏ ᴜꜱᴇᴅ ᴛᴏ ᴄʜᴇᴄᴋ ᴘʟᴀʏʟɪꜱᴛ
-- {DYNO_COMMANDK}group: ɢᴇᴛ ᴛʜᴇ ᴠᴄ ɴᴀᴍᴇ ᴡʜᴇʀᴇ ɪꜰ ʙᴏᴛ ʜᴀꜱ ᴊᴏɪɴᴇᴅ.
+•♪ `{DYNO_COMMANDK}sing`  ⇝Rᴇᴘʟʏ ᴡɪᴛʜ ᴀɴ ᴀᴜᴅɪᴏ ᴛᴏ ᴘʟᴀʏ/Qᴜᴇᴜᴇ ɪᴛ.
+•♪ `{DYNO_COMMANDK}sing`  ⇝Aʟꜱᴏ ᴜꜱᴇᴅ ᴛᴏ ᴄʜᴇᴄᴋ ᴘʟᴀʏʟɪꜱᴛ
 
 ⇜⊷°•♪  𝗔𝗱𝗺𝗶𝗻 𝗖𝗼𝗺𝗺𝗮𝗻𝗱𝘀  ♪•°⊶⇝
-- {DYNO_COMMANDK}plug : ᴄᴏᴍᴍᴀɴᴅ ʟɪᴋᴇ ᴀ ʙᴏꜱꜱ ᴛᴏ ᴊᴏɪɴ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴏꜰ ᴄᴜʀʀᴇɴᴛ ɢʀᴏᴜᴘ.
-- {DYNO_COMMANDK}off  : ʟᴇᴀᴠᴇ ᴄᴜʀʀᴇɴᴛ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴡʜᴇʀᴇ ɪꜱ ᴅᴊɪɴɢ.
-- {DYNO_COMMANDK}end  : ᴇɴᴅ ᴛʜᴇ ᴘʟᴀʏʟɪꜱᴛ ᴀɴᴅ ꜱᴛᴏᴘ ᴀʟʟ ᴍᴜꜱɪᴄ.
-- {DYNO_COMMANDK}pause: ᴘᴀᴜꜱᴇ!.
-- {DYNO_COMMANDK}resume: ʀᴇꜱᴜᴍᴇ!.
-- {DYNO_COMMANDK}replay: ᴘʟᴀʏ ꜰʀᴏᴍ ᴛʜᴇ ʙᴇɢɪɴɴɪɴɢ ᴏꜰ ᴄᴜʀʀᴇɴᴛ ᴍᴜꜱɪᴄ ʙᴇɪɴɢ ᴘʟᴀʏᴇᴅ.
-- {DYNO_COMMANDK}next : ꜱᴋɪᴘ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴍᴜꜱɪᴄ ᴏʀ ᴅᴇʟᴇᴛᴇ ᴀ ᴍᴜꜱɪᴄ ꜰʀᴏᴍ ᴘʟᴀʏʟɪꜱᴛ ʟɪᴋᴇ "skip 2".
-- {DYNO_COMMANDK}raw  : ᴄʟᴇᴀɴ ᴛʜᴇ ᴛᴇᴍᴘ ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ ᴍᴜꜱɪᴄ ʙʏ ʙᴏᴛc. 
-- {DYNO_COMMANDK}server: ᴛᴏ ᴄʜᴇᴄᴋ ᴛʜᴇ ᴘɪɴɢ ꜱᴛᴀᴛᴜꜱ ᴡɪᴛʜ ꜱᴇʀᴠᴇʀ.
-- {DYNO_COMMANDK}vol  : ᴄʜᴀɴɢᴇ ᴛʜᴇ ᴠᴏʟᴜᴍᴇ. ʟɪᴍɪᴛ 1-200
+•♪ `{DYNO_COMMANDK}group` ⇝Gᴇᴛ ᴛʜᴇ ᴠᴄ ɴᴀᴍᴇ ᴡʜᴇʀᴇ ɪꜰ ʙᴏᴛ ʜᴀꜱ ᴊᴏɪɴᴇᴅ.
+•♪ `{DYNO_COMMANDK}plug`  ⇝Cᴏᴍᴍᴀɴᴅ ʟɪᴋᴇ ᴀ ʙᴏꜱꜱ ᴛᴏ ᴊᴏɪɴ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴏꜰ ᴄᴜʀʀᴇɴᴛ ɢʀᴏᴜᴘ.
+•♪ `{DYNO_COMMANDK}exit`  ⇝Lᴇᴀᴠᴇ ᴄᴜʀʀᴇɴᴛ ᴠᴏɪᴄᴇ ᴄʜᴀᴛ ᴡʜᴇʀᴇ ɪꜱ ᴅᴊɪɴɢ.
+•♪ `{DYNO_COMMANDK}end`   ⇝Eɴᴅ ᴛʜᴇ ᴘʟᴀʏʟɪꜱᴛ ᴀɴᴅ ꜱᴛᴏᴘ ᴀʟʟ ᴍᴜꜱɪᴄ.
+•♪ `{DYNO_COMMANDK}pause` ⇝Pᴀᴜꜱᴇ!.
+•♪ `{DYNO_COMMANDK}resume`⇝Rᴇꜱᴜᴍᴇ!.
+•♪ `{DYNO_COMMANDK}replay`⇝Pʟᴀʏ ꜰʀᴏᴍ ᴛʜᴇ ʙᴇɢɪɴɴɪɴɢ ᴏꜰ ᴄᴜʀʀᴇɴᴛ ᴍᴜꜱɪᴄ ʙᴇɪɴɢ ᴘʟᴀʏᴇᴅ.
+•♪ `{DYNO_COMMANDK}next`  ⇝Sᴋɪᴘ ᴛʜᴇ ᴄᴜʀʀᴇɴᴛ ᴍᴜꜱɪᴄ ᴏʀ ᴅᴇʟᴇᴛᴇ ᴀ ᴍᴜꜱɪᴄ ꜰʀᴏᴍ ᴘʟᴀʏʟɪꜱᴛ ʟɪᴋᴇ "skip 2".
+•♪ `{DYNO_COMMANDK}raw`   ⇝Cʟᴇᴀɴ ᴛʜᴇ ᴛᴇᴍᴘ ᴅᴏᴡɴʟᴏᴀᴅᴇᴅ ᴍᴜꜱɪᴄ ʙʏ ʙᴏᴛc. 
+•♪ `{DYNO_COMMANDK}server`⇝Tᴏ ᴄʜᴇᴄᴋ ᴛʜᴇ ᴘɪɴɢ ꜱᴛᴀᴛᴜꜱ ᴡɪᴛʜ ꜱᴇʀᴠᴇʀ.
+•♪ `{DYNO_COMMANDK}vol`   ⇝Cʜᴀɴɢᴇ ᴛʜᴇ ᴠᴏʟᴜᴍᴇ. ʟɪᴍɪᴛ 1-200
 ᴛʜɪꜱ ᴘʀᴏᴊᴇᴄᴛ ʜᴀꜱ ʙᴇᴇɴ ʟɪᴄᴇɴꜱᴇᴅ ᴜɴᴅᴇʀ:[𝙂𝙉𝙐 𝙇𝙞𝙘𝙚𝙣𝙨𝙚](https://en.wikipedia.org/wiki/GNU_General_Public_License#:~:text=The%20GNU%20General%20Public%20License,share%2C%20and%20modify%20the%20software.&text=Prominent%20free%20software%20programs%20licensed,GNU%20Compiler%20Collection%20(GCC)"""
