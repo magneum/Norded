c44a222587b25efccd341874da4429ea746ff4da
HypeVoidSoul <hypevoidsoul@yahoo.com>
2021-08-16 22:53:09 +0530

    —••÷[🕊NӨЯDΣD🕊]÷••— added session generator

--- —••÷[🕊NOЯDΣD🕊]÷••—/__main__.py
+++ —••÷[🕊NOЯDΣD🕊]÷••—/__main__.py
@@ -170,11 +170,14 @@ file.write("def init():\n    global db\n    db = {}")
 file.close()
 import db
 db.init()
+import asyncio
 from db import db
+from pyromod import listen
 from youtube_search import YoutubeSearch
 from pyrogram.types import ChatPermissions
 from pyrogram.utils import MAX_CHANNEL_ID
 from pyrogram import Client, filters, idle
+from asyncio.exceptions import TimeoutError
 from pyrogram.raw.base import InputGroupCall
 from pytgcalls import GroupCall as NORDCALLER
 from pyrogram.types import Message as NordLink
@@ -185,7 +188,7 @@ from pyrogram.raw.functions.phone import EditGroupCallTitle
 from pyrogram.types import InlineKeyboardMarkup,InlineKeyboardButton
 from pyrogram.errors.exceptions.bad_request_400 import ChatAdminRequired
 from pyrogram.methods.messages.download_media import DEFAULT_DOWNLOAD_DIR
-
+from pyrogram.errors import SessionPasswordNeeded, FloodWait,PhoneNumberInvalid, ApiIdInvalid,PhoneCodeInvalid, PhoneCodeExpired
 
 
 
@@ -570,7 +573,24 @@ XERO_HELP = f"""{ӼɛӼօ}[𝗠𝗲𝗺𝗯𝗲𝗿_𝗖𝗼𝗺𝗺𝗮𝗻𝗱
 •♪ 𝘍𝘪𝘯𝘥 —••÷[🕊NOЯDΣD🕊]÷••—𝘏𝘌𝘙𝘖𝘒𝘜 𝘥𝘺𝘯𝘰 𝘶𝘴𝘢𝘨𝘦
 """
 
+PHONE_NUMBER_TEXT = """
+Now send your Telegram account's Phone number in International Format.Including Country code. 
+Example: **+919000000000**
 
+ᴘʀᴇꜱꜱ /exit ᴛᴏ ᴄᴀɴᴄᴇʟ ᴘʀᴏɢʀᴇꜱꜱ.
+"""
+
+OTP =  """
+÷•• ᴀɴ ᴏᴛᴘ ɪꜱ ꜱᴇɴᴛ ᴛᴏ ʏᴏᴜʀ ᴘʜᴏɴᴇ ɴᴜᴍʙᴇʀ
+
+÷•• ᴘʟᴇᴀꜱᴇ ᴇɴᴛᴇʀ ᴏᴛᴘ ɪɴ `1 2 3 4 5` [**DO NOT FORGET SPACES IN BETWEEN**]
+
+÷•• ɪꜰ 𝕡𝕪𝕣𝕠𝕘𝕣𝕒𝕞 ɴᴏᴛ ꜱᴇɴᴅɪɴɢ ᴏᴛᴘ ᴛʜᴇɴ ᴛʀʏ ᴛᴏ ʀᴇꜱᴛᴀʀᴛ ᴀɴᴅ ꜱᴛᴀʀᴛ ᴛᴀꜱᴋ ᴀɢᴀɪɴ ᴡɪᴛʜ /start ᴄᴏᴍᴍᴀɴᴅ ᴛᴏ ʙᴏᴛ.
+
+
+
+ᴘʀᴇꜱꜱ /exit ᴛᴏ ᴄᴀɴᴄᴇʟ ᴘʀᴏɢʀᴇꜱꜱ.
+"""
 
 
 """➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕
@@ -586,6 +606,722 @@ XERO_HELP = f"""{ӼɛӼօ}[𝗠𝗲𝗺𝗯𝗲𝗿_𝗖𝗼𝗺𝗺𝗮𝗻𝗱
 ➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕➕"""
 
 
+async def exit_work(hn, text: str):
+    if text.startswith("/exit"):
+        await hn.reply("Process Cancelled.")
+        return True
+    return False
+
+
+@NӨЯDBӨƬ.on_message(
+filters.private
+& filters.command("session"))
+async def norded(client, hn: NordLink):
+    try:
+        await hn.delete()
+        chat = hn.chat
+        HYPE_ASK_API = await hn.reply_photo(
+        photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+        caption=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+ɴᴏᴡ ꜱᴇɴᴅ ʏᴏᴜʀ `API_ID` ᴛᴏ ꜱᴛᴀʀᴛ ɢᴇɴᴇʀᴀᴛɪɴɢ —••÷[🕊NӨЯDΣD🕊]÷••— ᴘʏʀᴏɢʀᴀᴍ ꜱᴇꜱꜱɪᴏɴ ɴᴀᴍᴇ.
+
+
+**ɪꜰ ʏᴏᴜ ᴅᴏ ɴᴏᴛ ʜᴀᴠᴇ ᴇɪᴛʜᴇʀ ᴏꜰ ᴛʜᴏꜱᴇ ᴛʜᴇɴ ᴄʜᴇᴄᴋ ʙᴇʟᴏᴡ ʙᴏᴛ**
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+
+
+        HYPE_API = await NӨЯDBӨƬ.ask(
+        chat.id,
+        f"**÷••>**")
+
+
+        await HYPE_ASK_API.delete()
+
+
+        if await exit_work(hn, HYPE_API.text):
+            return
+
+
+        
+        try:
+            HYPE_API_CHECK = int(HYPE_API.text)
+        except Exception:
+            await hn.reply_photo(
+            photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+            caption=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+`API_ID` ɪꜱ ɪɴᴠᴀʟɪᴅ.
+ᴘʀᴇꜱꜱ /start ᴛᴏ ʀᴇꜱᴛᴀʀᴛ ᴇɴᴛɪʀᴇ ᴘʀᴏᴄᴇꜱꜱ ᴀɢᴀɪɴ.
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+            return
+
+        
+        api_id = HYPE_API.text
+        HYPE_ASK_HASK =  await hn.reply_photo(
+        photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+        caption=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+ɴᴏᴡ ꜱᴇɴᴅ ʏᴏᴜʀ `API_HASH`.
+
+ᴘʀᴇꜱꜱ /exit ᴛᴏ ᴄᴀɴᴄᴇʟ ᴘʀᴏɢʀᴇꜱꜱ.
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+
+        HYPE_HASH = await NӨЯDBӨƬ.ask(
+        chat.id,
+        f"**÷••>**")
+        await HYPE_ASK_HASK.delete()
+        await HYPE_API.delete()
+
+
+        
+        if await exit_work(hn, HYPE_HASH.text):
+            return
+
+
+        
+        if not len(HYPE_HASH.text) >= 30:
+            await hn.reply_photo(
+            photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+            caption=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+🔴 ＣＯＤＥ－ＲＥＤ 🔴
+`API_HASH` ɪꜱ ɪɴᴠᴀʟɪᴅ.
+ᴘʀᴇꜱꜱ /start ᴛᴏ ʀᴇꜱᴛᴀʀᴛ ᴇɴᴛɪʀᴇ ᴘʀᴏᴄᴇꜱꜱ ᴀɢᴀɪɴ.
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+            return
+
+
+
+        
+        api_hash = HYPE_HASH.text
+        while True:
+            number = await NӨЯDBӨƬ.ask(chat.id,PHONE_NUMBER_TEXT)
+            if not number.text:
+                continue
+            if await exit_work(hn, number.text):
+                return
+            phone = number.text
+            HYPE_ASK_Y = await hn.reply_photo(
+            photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+            caption=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+ɪꜱ ```{phone}``` ᴄᴏʀʀᴇᴄᴛ? (y/n)
+
+Ｓｅｎｄ： `y` 
+or
+Ｓｅｎｄ： `n` 
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+
+            confirm = await NӨЯDBӨƬ.ask(
+            chat.id,
+            f"**÷••>**")
+            await HYPE_HASH.delete()
+
+            if await exit_work(hn, confirm.text):
+                return
+            if "y" in confirm.text:
+                await  HYPE_ASK_Y.delete()
+                await confirm.delete()
+                break
+            
+
+
+
+        
+        try:
+            morphed = Client(
+            "my_account",
+            api_id=api_id,
+            api_hash=api_hash
+            )
+        except Exception as e:
+            await NӨЯDBӨƬ.send_photo(
+            chat.id,
+            photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+            text=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+🟡 ＣＯＤＥ－ＹＥＬＬＯＷ 🟡
+**ᴇʀʀᴏʀ:** `{str(e)}`
+ᴘʀᴇꜱꜱ /start ᴛᴏ ʀᴇꜱᴛᴀʀᴛ ᴇɴᴛɪʀᴇ ᴘʀᴏᴄᴇꜱꜱ ᴀɢᴀɪɴ.
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+            return
+
+
+        
+        try:
+            await morphed.connect()
+        except ConnectionError:
+            await morphed.disconnect()
+            await morphed.connect()
+
+
+
+        
+        try:
+            code = await morphed.send_code(phone)
+        except FloodWait as e:
+            await hn.reply_photo(
+            photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+            caption=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+ʏᴏᴜ ʜᴀᴠᴇ ꜰʟᴏᴏᴅᴡᴀɪᴛ ᴏꜰ {e.x} ꜱᴇᴄᴏɴᴅꜱ
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+            return
+
+
+        except ApiIdInvalid:
+            await hn.reply_photo(
+            photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+            caption=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+**ᴀᴘɪ ɪᴅ** ᴀɴᴅ **ᴀᴘɪ ʜᴀꜱʜ** ᴀʀᴇ ɪɴᴠᴀʟɪᴅ.
+
+ᴘʀᴇꜱꜱ /start ᴛᴏ ʀᴇꜱᴛᴀʀᴛ ᴇɴᴛɪʀᴇ ᴘʀᴏᴄᴇꜱꜱ ᴀɢᴀɪɴ.
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+            return
+
+
+        except PhoneNumberInvalid:
+            await hn.reply_photo(
+            photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+            caption=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+ʏᴏᴜʀ ᴘʜᴏɴᴇ ɴᴜᴍʙᴇʀ ɪꜱ ɪɴᴠᴀʟɪᴅ.
+
+ᴘʀᴇꜱꜱ /start ᴛᴏ ʀᴇꜱᴛᴀʀᴛ ᴇɴᴛɪʀᴇ ᴘʀᴏᴄᴇꜱꜱ ᴀɢᴀɪɴ.
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+            return
+
+
+
+        
+        try:
+            HYPE_OTP = await NӨЯDBӨƬ.ask(
+            chat.id,
+            OTP,
+            timeout=300)
+        except TimeoutError:
+            await hn.reply_photo(
+            photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+            caption=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+ᴛɪᴍᴇ ʟɪᴍɪᴛ ʀᴇᴀᴄʜᴇᴅ ᴏꜰ 5 ᴍɪɴ.
+
+ᴘʀᴇꜱꜱ /start ᴛᴏ ʀᴇꜱᴛᴀʀᴛ ᴇɴᴛɪʀᴇ ᴘʀᴏᴄᴇꜱꜱ ᴀɢᴀɪɴ.
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+            return
+
+
+
+          
+        if await exit_work(hn, HYPE_OTP.text):
+            return
+
+
+
+        
+        HYPE_otp_CODE = HYPE_OTP.text
+        try:
+            await morphed.sign_in(
+            phone,
+            code.phone_code_hash,
+            phone_code=' '.join(str(HYPE_otp_CODE)))
+
+        except PhoneCodeInvalid:
+            await hn.reply_photo(
+            photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+            caption=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+🟡 ＣＯＤＥ－ＹＥＬＬＯＷ 🟡
+ɪɴᴠᴀʟɪᴅ ᴄᴏᴅᴇ.
+
+
+ᴘʀᴇꜱꜱ /start ᴛᴏ ʀᴇꜱᴛᴀʀᴛ ᴇɴᴛɪʀᴇ ᴘʀᴏᴄᴇꜱꜱ ᴀɢᴀɪɴ.
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+            return
+
+
+        except PhoneCodeExpired:
+            await hn.reply_photo(
+            photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+            caption=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+🟡 ＣＯＤＥ－ＹＥＬＬＯＷ 🟡
+ᴄᴏᴅᴇ ɪꜱ **𝐄𝐗𝐏𝐈𝐑𝐄𝐃** ᴏʀ ʏᴏᴜ ꜰᴏʀɢᴏᴛ ᴛᴏ **𝐏𝐔𝐓 𝐏𝐑𝐎𝐏𝐄𝐑 𝐒𝐏𝐀𝐂𝐄𝐒**
+
+ᴘʀᴇꜱꜱ /start ᴛᴏ ʀᴇꜱᴛᴀʀᴛ ᴇɴᴛɪʀᴇ ᴘʀᴏᴄᴇꜱꜱ ᴀɢᴀɪɴ.
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+            return
+
+
+        except SessionPasswordNeeded:
+            try:
+                await hn.reply_photo(
+                photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+                caption=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+ʏᴏᴜʀ ᴀᴄᴄᴏᴜɴᴛ ʜᴀᴠᴇ **𝐓𝐰𝐨-𝐒𝐭𝐞𝐩 𝐕𝐞𝐫𝐢𝐟𝐢𝐜𝐚𝐭𝐢𝐨𝐧.**
+ᴘʟᴇᴀꜱᴇ ᴇɴᴛᴇʀ ʏᴏᴜʀ ᴘᴀꜱꜱᴡᴏʀᴅ.
+
+ᴘʀᴇꜱꜱ /exit ᴛᴏ ᴄᴀɴᴄᴇʟ ᴘʀᴏɢʀᴇꜱꜱ.
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+                HYPE_PASSCODE = await NӨЯDBӨƬ.ask(chat.id,f"**÷••>**",timeout=300)
+
+
+            except TimeoutError:
+                await hn.reply_photo(
+                photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+                caption=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+`ᴛɪᴍᴇ ʟɪᴍɪᴛ ʀᴇᴀᴄʜᴇᴅ ᴏꜰ 5 ᴍɪɴ`
+
+ᴘʀᴇꜱꜱ /start ᴛᴏ ʀᴇꜱᴛᴀʀᴛ ᴇɴᴛɪʀᴇ ᴘʀᴏᴄᴇꜱꜱ ᴀɢᴀɪɴ.`
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+                return
+
+
+        
+            if await exit_work(hn,HYPE_PASSCODE.text):
+                return
+
+
+
+        
+            HYPE_GOT_CODE = HYPE_PASSCODE.text
+            try:
+                await morphed.check_password(HYPE_GOT_CODE)
+            except Exception as e:
+                await hn.reply_photo(
+                photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+                caption=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+🟡 ＣＯＤＥ－ＹＥＬＬＯＷ 🟡
+**ᴇʀʀᴏʀ:** `{str(e)}`
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+                return
+
+        
+        except Exception as e:
+            await NӨЯDBӨƬ.send_photo(
+            chat.id,
+            photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+            text=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+🟡 ＣＯＤＥ－ＹＥＬＬＯＷ 🟡
+**ᴇʀʀᴏʀ:** `{str(e)}`
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+            return
+
+
+        try:
+            SESSION_HYPED = await morphed.export_NAMER_MAKERion_string()
+            await morphed.send_photo(
+            "me",
+            photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+            caption=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+ʜᴇʀᴇ ɪꜱ ʏᴏᴜʀ 𝘽𝙖𝙨𝙞𝙘 𝙋𝙮𝙧𝙤𝙜𝙧𝙖𝙢 𝙎𝙚𝙨𝙨𝙞𝙤𝙣 𝙉𝙖𝙢𝙚:
+
+```{SESSION_HYPED}```
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+
+
+        
+            await morphed.disconnect()
+            await NӨЯDBӨƬ.send_photo(
+            chat.id,
+            photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+            caption=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+`𝘚𝘵𝘳𝘪𝘯𝘨 𝘨𝘦𝘯𝘦𝘳𝘢𝘵𝘪𝘰𝘯 𝘸𝘢𝘴 𝘚𝘶𝘤𝘤𝘦𝘴𝘴𝘧𝘶𝘭𝘭𝘺 𝘤𝘰𝘮𝘱𝘭𝘦𝘵𝘦𝘥`
+ᴄʜᴇᴄᴋ ʏᴏᴜʀ ꜱᴀᴠᴇᴅ ᴍᴇꜱꜱᴀɢᴇꜱ
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+
+
+        
+        except Exception as e:
+            await NӨЯDBӨƬ.send_photo(
+            chat.id,
+            photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+            caption=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+🟡 ＣＯＤＥ－ＹＥＬＬＯＷ 🟡
+**ᴇʀʀᴏʀ:** `{str(e)}`
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+            return
+
+
+
+        
+    except Exception as e:
+        await NӨЯDBӨƬ.send_photo(
+        chat.id,
+        photo="https://telegra.ph/file/0592f028e92ff2a8e73f0.jpg",
+        caption=f"""
+**—••÷🧊 SΣSSIӨП_MΛKΣЯ 🧊÷••—** `by` 🚀🔥 ΉYPΣ VӨID LΛB 🔥🚀
+
+
+🟡 ＣＯＤＥ－ＹＥＬＬＯＷ 🟡
+**ᴇʀʀᴏʀ:** `{str(e)}`
+
+—••÷[🕊NӨЯDΣD🕊]÷••—
+""",
+    reply_markup = InlineKeyboardMarkup([[
+    InlineKeyboardButton(
+    text="🏷Group",
+    url="https://t.me/HYPEVOIDS"),],[
+    InlineKeyboardButton(
+    text="—••÷[🕊NӨЯDΣD🕊]÷••—",
+    url="https://t.me/nordedbot"),],[
+    InlineKeyboardButton(
+    text="💰Channel",
+    url="https://t.me/HYPEVOIDLAB"),],[
+    InlineKeyboardButton(
+    text="⚜️Dev+Git",
+    url="https://t.me/HYPEVOIDBOT")
+    ]]))
+        return
 
 
 """
